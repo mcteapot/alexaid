cd116582c52b1613e240fdac337e5233e52e4055
Seiji Tanaka <seiji.t@gmail.com>
2013-05-16 02:49:59 -0700

    added particles

--- js/main.js
+++ js/main.js
@@ -17,4 +17,18 @@ jQuery(document).ready(function($) {
     TweenMax.to(badgeObject, 1, {transformMatrix:{scaleX:2, scaleY:2, rotation:30}});
 
    //TweenMax.to(badgeObject, -1, {rotation:"360", ease:Linear.easeNone, repeat:10});
+    spawnParticles();
+
+   	function spawnParticles(){
+   		for(var i = 0; i<100; i++){
+    		var ptc = $("<div class='particle'></div>").appendTo('body');
+    		var top = Math.floor(Math.random()*window.screen.availHeight);
+    		var left = Math.floor(Math.random()*window.screen.availWidth);
+    		var opacity = Math.random();
+    		// $(ptc).css({position: "absolute", width: "4px", height: "4px",top:top, left: left});
+    		var targetTop = top - 800;
+    		var floatDuration = 150000 - 120000*(opacity);
+    		$(ptc).css({top:top, left: left, opacity: opacity}).animate({top: targetTop}, floatDuration, "linear");
+   		}
+    }
 });
