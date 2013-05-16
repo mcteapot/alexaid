3e6d1ad813f6c674c1a19d5ee1b699de51c563c1
Arjun Prakash <archieoi@gmail.com>
2013-05-16 01:37:14 -0700

    tweenmax test

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
 
+jQuery(document).ready(function($) {
+    // $ inside here refers to jQuery
+    console.log("Document READY")
+    var tempObject = $(".graphic")[0];
+    TweenMax.to(tempObject, 0, {scale:0.2, opacity:0.3});
+    TweenMax.to(tempObject, .4, { scale:1, opacity:1});
+});
