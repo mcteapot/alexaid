37b9e896f69bb77bf69446ca4ed84c57756bdefa
Arjun Prakash <archieoi@gmail.com>
2013-05-16 01:54:18 -0700

    links out

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 
 jQuery(document).ready(function($) {
     // $ inside here refers to jQuery
+
+    // set links
+    $('#event-location.address').click(function() {
+ 		window.open('http://maps.google.com/?q=8714 W Washington Blvd Culver City, CA 90232');
+ 		console.log("OPEN");
+  	});
+
     console.log("Document READY")
     var tempObject = $(".graphic")[0];
     TweenMax.to(tempObject, 0, {scale:0.2, opacity:0.3});
